d8ce9c8638c649f3c0958995c6c6dfaf7834b90b
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-04-15 22:58:11 -0300

    feat: logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import { RocketIcon } from "@radix-ui/react-icons";
+
+export function Logo() {
+  return (
+    <div className="w-8 h-8 flex items-center justify-center rounded-md bg-neutral-100">
+      <RocketIcon className="w-5 h-5 text-zinc-900" />
+    </div>
+  );
+}
