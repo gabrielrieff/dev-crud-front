84ce959edee2942936d273ed5b4dd684d9f8d597
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-05-06 12:52:05 -0300

    feat: Adicionado força de senha

--- src/app/login/_components/schemaRegister.ts
+++ src/app/login/_components/schemaRegister.ts
@@ -6,8 +6,19 @@ export const useSchemaRegister = () => {
   const schema = z.object({
     first_name: z.string(),
     last_name: z.string(),
-    email: z.string(),
-    password: z.string(),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(7, "A senha deve ter pelo menos 7 caracteres")
+      .refine((value) => /[A-Z]/.test(value), {
+        message: "A senha deve conter pelo menos uma letra maiúscula",
+      })
+      .refine((value) => /\d/.test(value), {
+        message: "A senha deve conter pelo menos um número",
+      })
+      .refine((value) => /[!@#$%^&*()_+]/.test(value), {
+        message: "A senha deve conter pelo menos um caractere especial",
+      }),
   });
 
   type formDataProps = z.infer<typeof schema>;
